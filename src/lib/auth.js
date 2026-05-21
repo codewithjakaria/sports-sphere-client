ecc97d78fe0193031a13ab517bf0d44a8b16ee6d
Your Name <your-email@gmail.com>
2026-05-21 21:18:32 +0600

    Vercel deploy issue

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,8 +1,22 @@
+// import { betterAuth } from 'better-auth';
+// import { mongodbAdapter } from 'better-auth/adapters/mongodb';
+// import { MongoClient } from 'mongodb';
+
+// const client = new MongoClient(process.env.MONGODB_URI);
+// const db = client.db('Sports-Sphere');
+
+// export const auth = betterAuth({
+//   database: mongodbAdapter(db),
+//   emailAndPassword: {
+//     enabled: true,
+//   },
+// });
+
 import { betterAuth } from 'better-auth';
 import { mongodbAdapter } from 'better-auth/adapters/mongodb';
 import { MongoClient } from 'mongodb';
 
-
+// ১. ক্লায়েন্টটি গ্লোবালি না রেখে চেক করে নিন
 const client = new MongoClient(process.env.MONGODB_URI);
 const db = client.db('Sports-Sphere');
 
@@ -11,4 +25,7 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  // ২. অবশ্যই baseURL টি এখানে ডিফাইন করে দিন
+  baseURL: process.env.BETTER_AUTH_URL,
+  secret: process.env.BETTER_AUTH_SECRET,
 });
