bb09cd49aec6b0b098a5b763748a51ac3e2e899c
Your Name <your-email@gmail.com>
2026-05-23 23:35:08 +0600

    fix: added trustedOrigins

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,18 +2,15 @@ import { betterAuth } from 'better-auth';
 import { mongodbAdapter } from 'better-auth/adapters/mongodb';
 import { MongoClient } from 'mongodb';
 
-// ১. মঙ্গোডিবি ক্লায়েন্ট কানেকশন
 const client = new MongoClient(process.env.MONGODB_URI);
-const db = client.db('Sports-Sphere'); // আপনার ডাটাবেজ নাম
+const db = client.db('Sports-Sphere');
 
-// ২. অথেন্টিকেশন কনফিগারেশন
 export const auth = betterAuth({
   database: mongodbAdapter(db),
-
-  // ইমেইল এবং পাসওয়ার্ড অথেন্টিকেশন চালু করা
   emailAndPassword: {
     enabled: true,
   },
   baseURL: process.env.BETTER_AUTH_URL,
   secret: process.env.BETTER_AUTH_SECRET,
+  trustedOrigins: [process.env.CLIENT_URL],
 });
