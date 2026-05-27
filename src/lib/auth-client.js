1d5df0f735d31e6fbe3c87ef52245de890a2791f
Your Name <your-email@gmail.com>
2026-05-27 11:39:58 +0600

    Login issue fix

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,15 +1,11 @@
-// import { createAuthClient } from 'better-auth/react';
-
-// export const authClient = createAuthClient({
-//   baseURL: process.env.NEXT_PUBLIC_API_URL,
-// });
-
-// export const { signIn, signUp, signOut, useSession } = authClient;
-
 import { createAuthClient } from 'better-auth/react';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: isProduction
+    ? process.env.NEXT_PUBLIC_API_URL
+    : 'http://localhost:5001',
 
   fetchOptions: {
     credentials: 'include',
