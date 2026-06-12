23edc98b96e67252e79fa0f7ae556880c297e42d
Your Name <your-email@gmail.com>
2026-06-12 23:01:20 +0600

    Deployment issue

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,12 +1,27 @@
+// import { createAuthClient } from 'better-auth/react';
+
+// export const authClient = createAuthClient({
+//   baseURL: process.env.NEXT_PUBLIC_API_URL + '/api/auth',
+//   fetchOptions: {
+//     credentials: 'include',
+//   },
+// });
+
+// export const { signIn, signUp, signOut, useSession } = authClient;
+
 import { createAuthClient } from 'better-auth/react';
 
+const baseURL = process.env.NEXT_PUBLIC_API_URL;
+
+if (!baseURL) {
+  throw new Error('NEXT_PUBLIC_API_URL is missing');
+}
+
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_API_URL + '/api/auth',
+  baseURL: `${baseURL}/api/auth`,
   fetchOptions: {
     credentials: 'include',
   },
 });
-
-
 
 export const { signIn, signUp, signOut, useSession } = authClient;
