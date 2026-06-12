aa1a992a678c98ab3939f8980bf2a66b34bc8709
Your Name <your-email@gmail.com>
2026-06-12 23:21:09 +0600

    Problem fix

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,14 +1,3 @@
-// import { createAuthClient } from 'better-auth/react';
-
-// export const authClient = createAuthClient({
-//   baseURL: process.env.NEXT_PUBLIC_API_URL + '/api/auth',
-//   fetchOptions: {
-//     credentials: 'include',
-//   },
-// });
-
-// export const { signIn, signUp, signOut, useSession } = authClient;
-
 import { createAuthClient } from 'better-auth/react';
 
 const baseURL = process.env.NEXT_PUBLIC_API_URL;
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,32 +1,3 @@
-// import { betterAuth } from 'better-auth';
-// import { mongodbAdapter } from 'better-auth/adapters/mongodb';
-// import { MongoClient } from 'mongodb';
-
-// const client = new MongoClient(process.env.MONGODB_URI);
-// const db = client.db('Sports-Sphere');
-
-// export const auth = betterAuth({
-//   database: mongodbAdapter(db),
-
-//   baseURL: process.env.BETTER_AUTH_URL,
-
-//   trustedOrigins: [
-//     'http://localhost:3000',
-//     'http://sports-sphere-client-phi.vercel.app',
-//   ],
-
-//   emailAndPassword: {
-//     enabled: true,
-//   },
-
-//   socialProviders: {
-//     google: {
-//       clientId: process.env.GOOGLE_CLIENT_ID,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//     },
-//   },
-// });
-
 import { betterAuth } from 'better-auth';
 import { mongodbAdapter } from 'better-auth/adapters/mongodb';
 import { MongoClient } from 'mongodb';
@@ -35,7 +6,6 @@ const client = new MongoClient(process.env.MONGODB_URI);
 await client.connect();
 
 const db = client.db('Sports-Sphere');
-
 
 if (!process.env.BETTER_AUTH_URL) {
   throw new Error(' BETTER_AUTH_URL is missing in environment');
